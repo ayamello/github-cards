05403f6a7776fe665cce2c058aba503906b567a5
Ayana Mello <ayanamello27@gmail.com>
2021-07-20 23:31:12 -0300

    Add style to SearchForm

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,9 +1,29 @@
 import { TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
+const useStyle = makeStyles(theme => ({
+    root: {
+        width: "100%",
+        height: "100vh",
+        textAlign: "center",
+        marginTop: 100 
+    },
+    input: {
+        width: 300,
+    },
+    button: {
+        fontSize: 12,
+        height: 40,
+        marginLeft: -10,
+        borderRadius: 0
+    }
+}));
+
 function SearchForm() {
+    const classes = useStyle();
 
     const schema = yup.object().shape({
         repositorio: yup.string().required("Campo obrigatório")
@@ -20,7 +40,7 @@ function SearchForm() {
     }
 
     return(
-        <form onSubmit={handleSubmit(handleSearch)}>
+        <form onSubmit={handleSubmit(handleSearch)} className={classes.root}>
             <TextField 
                 label="usuario/repo"
                 variant="outlined"
@@ -28,8 +48,13 @@ function SearchForm() {
                 {...register("repositorio")}
                 error={!!errors.repositorio}
                 helperText={errors.repositorio?.message}
+                className={classes.input}
             />
-            <Button type="submit" variant="contained" color="primary">Pesquisar</Button>
+            <Button 
+                type="submit" 
+                variant="contained" 
+                color="primary" 
+                className={classes.button}>Pesquisar</Button>
         </form>
     );
 }
